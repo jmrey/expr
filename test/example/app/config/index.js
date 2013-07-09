672efea786eac0e3d3a77e7711b479fa02e3d8c0
JMRey <mue.navarone@gmail.com>
2013-07-09 14:25:29 -0500

    configuration for tests.

---
+++ test/example/app/config/index.js
@@ -0,0 +1,15 @@
+exports.dev = {
+  port: 3000,
+  logs : {
+    console : true,
+    level : "info"
+  }
+};
+
+exports.production = {
+  port: 3000,
+  logs : {
+    console : true,
+    level : "info"
+  }
+};
