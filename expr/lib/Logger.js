d2bb9bf7e964b0635e8eb1dc136d5bd8274ce6d2
JMRey <mue.navarone@gmail.com>
2013-07-11 10:38:31 -0500

    Logger is now winston; inspectData renamed to dump.

--- expr/lib/Logger.js
+++ expr/lib/Logger.js
@@ -1,47 +1,23 @@
 /* jslint node: true */
 "use strict";
 
-var winston = require('winston')
-  , eyes = require('winston/node_modules/eyes')
-  , inspect = eyes.inspector({ stream: null,
-    styles: {               // Styles applied to stdout
-      all:     null,        // Overall style applied to everything
-      label:   'underline', // Inspection labels, like 'array' in `array: [1, 2, 3]`
-      other:   'inverted',  // Objects which don't have a literal representation, such as functions
-      key:     'yellow',      // The keys in object literals, like 'a' in `{a: 1}`
-      special: 'red',      // null, undefined...
-      number:  'cyan',      // 1, 2, 3, etc
-      bool:    'magenta',   // true false
-      regexp:  'green',     // /\d+/
-      string:  'green'
-    }
-  });
+var util = require('util')  
+  , winston = require('winston');
 
-function Logger(config) {
-  winston.cli();
-  try {
-    winston.remove(winston.transports.Console);
-  } catch (ex) {
-    console.log('EXCEPTION: Can\'t remove CONSOLE transport on Winston...');
-  }
-  if (config.logs && config.logs.console) {
-    winston.add(winston.transports.Console, {level: config.logs.level, colorize: true});
-  }
-
-  winston.info('Winston is now logging.');
-  winston.inspect = inspectData;
-  return winston;
-}
 
 /*
- * @method  inspectData Log an javascript object.
+ * @method  dump Log an javascript object.
  * @param   {Object}  object  Obecjt to Inspect.
  * @param   {String}  label   Label for data log.
  * @return  null
  */
-function inspectData(obj, label) {
-  var keys = Object.keys(obj),
-      list = inspect(obj);
+var dump = function (obj, label) {
+  if ('string' === typeof obj) {
+    winston.data(obj.name + " " + obj);
+  }
+
+  var keys = Object.keys(obj)
+    , list = require('util').inspect(obj, {colors: true});
   
   label = label || 'Inspecting Object';
   
@@ -55,7 +31,24 @@ function inspectData(obj, label) {
   list.split('\n').forEach(function (line) {
     winston.data(line);
   });
-}
+};
 
-module.exports = Logger;
+module.exports = function (config) {
+  winston.cli();
+  try {
+    winston.remove(winston.transports.Console);
+  } catch (ex) {
+    console.log('EXCEPTION: Can\'t remove CONSOLE transport on Winston...');
+  }
+
+  if (config.logs && config.logs.console) {
+    winston.add(winston.transports.Console, {level: config.logs.level, colorize: true});
+  }
+
+  winston.dump = dump;
+  winston.info('Winston is now logging.');
+
+
+  return winston;
+};
 
