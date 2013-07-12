f131cde683305b8748f5d3bf5f973a6e98fc6615
JMRey <mue.navarone@gmail.com>
2013-07-12 13:21:49 -0500

    check if obj is object or function in dump.

--- expr/lib/Logger.js
+++ expr/lib/Logger.js
@@ -12,25 +12,26 @@ var util = require('util')
  * @return  null
  */
 var dump = function (obj, label) {
-  if ('string' === typeof obj) {
-    winston.data(obj.name + " " + obj);
-  }
+  label = label || ('Inspecting ' + typeof obj);
 
-  var keys = Object.keys(obj)
-    , list = require('util').inspect(obj, {colors: true});
-  
-  label = label || 'Inspecting Object';
-  
-  if (keys.length === 0) {
-    return winston.warn('Empty Object');
+  if (obj && ('object' === typeof obj || 'function' === typeof obj)) {
+    var keys = Object.keys(obj)
+      , list = require('util').inspect(obj, {colors: true});
+    
+    if (keys.length === 0) {
+      return winston.warn('Empty Object');
+    } else {
+      list = list.replace(/\n\s{2}/ig, '\n  ');
+    }
+
+    winston.data(label);
+    list.split('\n').forEach(function (line) {
+      winston.data(line);
+    });
   } else {
-    list = list.replace(/\n\s{2}/ig, '\n  ');
+    winston.data(label);
+    winston.data(obj);
   }
-
-  winston.data(label);
-  list.split('\n').forEach(function (line) {
-    winston.data(line);
-  });
 };
 
 module.exports = function (config) {
