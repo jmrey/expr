b42197ec2ccd2df40d2f5dc6f9416c8c6e81be0c
JMRey <mue.navarone@gmail.com>
2013-07-23 16:35:48 -0500

    socket.io configuration.

--- expr/lib/EventHandler.js
+++ expr/lib/EventHandler.js
@@ -1,8 +1,30 @@
 /* jslint node: true */
 "use strict";
 
-var io = require('socket.io');
-
 module.exports = function (instance) {
-  return io.listen(instance.server);
+  var io = require('socket.io').listen(instance.server);
+
+  io.configure(function () {
+    io.set('authorization', function (handshake, fn) {
+      if (handshake.headers.cookie) {
+        var cookie = instance.utils.parseCookie(handshake.headers.cookie);
+        var sessionID = instance.config.session.prefix + '_' + cookie[instance.config.session.name];
+        handshake.cookie = cookie;
+
+        instance.sessionStore.get(sessionID, function(err, session) {
+          if (err || !session) {
+            fn('Could not found Session.', false);
+          } else {
+            handshake.session = JSON.parse(session);
+            handshake.sessionID = sessionID;
+            fn(null, true);
+          }
+        });
+      } else {
+        return fn('No cookie transmitted.', false);
+      }
+    });  
+  });
+  
+  return io;
 };
